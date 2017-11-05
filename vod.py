b5f27e5456c713fe451f44d98439d4c632d80ddd
Tuvia Lerea <tuvia.lerea@gmail.com>
2017-11-05 01:27:48 -0400

    added multi-threading, seems to work much faster

--- vod.py
+++ vod.py
@@ -1,8 +1,16 @@
+from __future__ import print_function
+from simple_requests import Requests
 import requests
 import json
 import argparse
 import os
 import shutil
+import copy
+import re
+
+def getFileName(url):
+	parts = url.split('/')
+	return parts[len(parts) - 1]
 
 def getnauthTokens(id):
 	pattern = 'https://api.twitch.tv/api/vods/{0}/access_token'
@@ -23,14 +31,15 @@ def getm3u(id):
 	r = requests.get(url)
 	if r.status_code != 200:
 		raise Exception("API returned {0}".format(r.status_code))
-	print url
+	print(url)
 	return r
 
 def getLinkFromm3u(m3u):
 	for line in m3u.iter_lines():
-		if ('high' in line  or '1080p60' in line or '720p60' in line) and not line.startswith('#'):
+		if ('high' in line  or '1080p60' in line or '720p60' in line or '720p30' in line) and not line.startswith('#'):
 			return line
 	raise Exception("No high quality link")
+
 def truncLink(url):
 	if 'high' in url:
 		split = url.partition('high')
@@ -38,11 +47,13 @@ def truncLink(url):
 		split = url.partition('1080p60')
 	elif '720p60' in url:
 		split = url.partition('720p60')
+	elif '720p30' in url:
+		split = url.partition('720p30')
 	return split[0] + split[1] + '/'
 
 def getAlltsLinks(url):
 	prefix = truncLink(url)
-	print prefix
+	print(prefix)
 	r = requests.get(url)
 	links = []
 	if r.status_code != 200:
@@ -56,31 +67,40 @@ def getAlltsLinks(url):
 		links.append(fullPath)
 	return links
 
-def downloadTS(id, links, path):
-	count = 0
-	listFile = open(path + 'list.m3u8', 'w')
-	for link in links:
-		r = requests.get(link)
-		filename = path + id + '_' + str(count) + '.ts'
-		with open(filename, 'wb') as output:
-			print 'Writing' + str(count)
-			output.write(r.content)
-		count = count + 1
-		lineToWrite = "file '" + filename + "'\n"
-		listFile.write(lineToWrite)
+def getAllTS(id, links, path):
+	requests = Requests(concurrent=10)
+	for response in requests.swarm(links, maintainOrder=False):
+			writeTS(path, response)
+
+def writeTS(path, response):
+	filename = path + getFileName(response.url)
+	with open(filename, 'wb') as output:
+		print('Writing ' + filename)
+		output.write(response.content)
+
+def createm3u8(path):
+	files = []
+	for file in sorted(os.listdir(path), key=lambda x: (int(re.sub('\D','',x)),x)):
+		files.append("file '" + file + "'\n")
+	with open(path + '/list.m3u8', 'w') as m3u8file:
+		for line in files:
+			m3u8file.write(line)
 
 def combine(source, dest):
-	print "Combining"
+	print('Combining')
 	os.system('ffmpeg -f concat -safe 0 -i ' + source + 'list.m3u8 -bsf:a aac_adtstoasc -c copy ' + dest + 'output.mp4')
+
 
 
 def downloadVod(args):
 	m3u = getm3u(args.id)
+	print(m3u.text)
 	m3u8 = getLinkFromm3u(m3u)
 	links = getAlltsLinks(m3u8)
 	if not os.path.exists(args.path + 'tmp'):
 		os.mkdir(args.path + 'tmp')
-	downloadTS(args.id, links, (args.path + 'tmp/'))
+	getAllTS(args.id, links, (args.path + 'tmp/'))
+	createm3u8(args.path + 'tmp')
 	combine((args.path + 'tmp/'), args.path)
 	shutil.rmtree(args.path + 'tmp/')
 
